6a2c81cf693f6b6266829eaf1c01457355864293
RYArrowsmith <RYArrowsmith@gmail.com>
2017-03-06 03:50:51 +0000

    Fixing ping, part 1

--- src/main/java/net/chromaryu/discordgaming/commands/register.java
+++ src/main/java/net/chromaryu/discordgaming/commands/register.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  * Created by midgard on 17/03/06.
  */
 public class register {
-    public static void register(User u) throws IOException {
+    public static void register(User u, Message m) throws IOException {
         File f = new File("char/");
         File charFile = new File("char/"+u.getName()+"#"+u.getDiscriminator()+".json");
         f.mkdir();
@@ -22,17 +22,21 @@ public class register {
             objectMapper.writeValue(charFile, new PlayerInfoLoader(u));
             u.openPrivateChannel().queue(
                     privateChannel -> {
-                        long time = System.currentTimeMillis();
-                        EmbedBuilder eb = new EmbedBuilder();
-                        eb.setColor(new Color(255, 102, 25));
-                        eb.addField("Welcome to Dungeon Master!", "Hello. Your character have been made!", true);
-                        eb.addField("Password", "Current password is your current username **" + u.getName() + "**!", true);
-                        eb.setTitle("Dungeon Master Notice", null);
-                        eb.setFooter("Done in " + (System.currentTimeMillis() - time) + "ms", null);
-                        privateChannel.sendMessage(eb.build()).queue();
-                    }
-            );
+                        privateChannel.sendMessage("Loading...").queue(
+                            loading -> {
+                                
+                                EmbedBuilder eb = new EmbedBuilder();
+                                eb.setColor(new Color(255, 102, 25));
+                                eb.addField("Welcome to Dungeon Master!", "Hello. Your character have been made!", true);
+                                eb.addField("Password", "Your current password is your current username **" + u.getName() + "**!", true);
+                                eb.setTitle("Dungeon Master Notice", null);
+                                eb.setFooter("Done in " + m.getCreationTime().until(loading.getCreationTime(), ChronoUnit.MILLIS) + "ms", null);
+                                loading.editMessage(eb.build()).queue();
+                            });     
+                    });
         }
     }
+}
+       
 }
 
